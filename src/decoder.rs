870cf3a9f7c18c61730cb7c1eee3d67d6787e44e
unic0rn9k <unic0rn9k@protonmail.com>
2024-02-25 14:11:23 +0100

    Tried to implement batching

--- src/decoder.rs
+++ src/decoder.rs
@@ -1,7 +1,10 @@
 //! # Basic decoder block
 //! - [X] Optional tril mask
 //! - [ ] Positional embeddings
+//! - [-] Batching
 //! - [ ] Multiple hheads
+
+use std::io::Read;
 
 use crate::*;
 
@@ -37,7 +40,7 @@ impl DecoderBlock{
 
 impl Module for DecoderBlock{
 
-    /// btach x head x seqd x embd
+    /// batch x head x seqd x embd
     fn forward(&self, xs: &Tensor) -> Result<Tensor, Error>{
         let dev = xs.device();
         let seqd = xs.shape().dims().iter().rev().skip(1).next().unwrap();
@@ -52,7 +55,7 @@ impl Module for DecoderBlock{
 
         let tril = Tensor::from_vec(tril.to_vec2().unwrap().into_iter().flatten().map(|n: f32| if n == 0.{-f32::INFINITY}else{0.} ).collect(), tril.shape(), dev).unwrap();
 
-        let bruh = if self.conf.tril { (tril + scores).unwrap() }else{ scores };
+        let bruh = if self.conf.tril { (tril.repeat((40, 1, 1)) + scores).unwrap() }else{ scores };
         let scores = softmax(&bruh, D::Minus1).unwrap();
         //println!("bruh: {:?}", bruh.to_vec2::<f32>());
         //println!();
@@ -72,6 +75,8 @@ pub fn simple_llm(vs: VarBuilder) -> impl Module{
 }
 
 pub fn main() -> Result<()>{
+    let batch_size = 40;
+
     let dev = Device::Cpu;
 
     let varmap = VarMap::new();
@@ -86,34 +91,46 @@ pub fn main() -> Result<()>{
 
     let mut opt = AdamW::new_lr(vars, 0.01)?;
 
-    let mut file = BufReader::new(File::open("tiny-shakespeare.txt")?).lines();
+    let mut file = BufReader::new(File::open("tiny-shakespeare.txt")?);
     let mut cost_sum = 0.;
     for n in 0..{
-        let line = file.next().expect("EOF")?;
-        let tokens = line.as_bytes();
-        let seqd = tokens.len();
+        let seqd = n % 80 + 2;
+
+        let mut ys = vec![];
+        let mut xs = vec![];
+
+        for _n in 0..batch_size{
+            let mut tokens = vec![0u8; seqd+1];
+            file.read_exact(&mut tokens).expect("EOF");
+
+            ys.push(Tensor::new(&tokens[1..seqd+1], &dev)?);
+            let toks = Tensor::new(&tokens[0..seqd], &dev)?;
+
+            xs.push(embeddings.as_tensor().embedding(&toks).unwrap().reshape((1, seqd, EMBD)).unwrap());
+        }
 
         if seqd < 2{
             continue
         }
 
-        let ys = Tensor::new(&tokens[1..seqd], &dev)?;
-        let tokens = Tensor::new(&tokens[0..seqd-1], &dev)?;
+        let xs = Tensor::cat(&xs, 0).unwrap();
+        //let ys = Tensor::cat(&ys, 0).unwrap();
 
-        //let ys = one_hot(ys, 255, 1., 0.).unwrap();
-
-        let xs = embeddings.as_tensor().embedding(&tokens)?;
         let yhat = decoder.forward(&xs).unwrap();
 
         let log_sm = log_softmax(&yhat, D::Minus1)?;
 
-        let loss = loss::nll(&log_sm, &ys).unwrap();
-        cost_sum += loss.to_scalar::<f32>().unwrap();
+        let mut loss = Tensor::new(0f32, &dev).unwrap();
+        for n in 0..batch_size{
+            loss = (loss + loss::nll(&log_sm.get_on_dim(0, n).unwrap(), &ys[n]).unwrap()).unwrap();
+            cost_sum += loss.to_scalar::<f32>().unwrap();
+        }
+        loss = (loss / Tensor::new(batch_size as f32, &dev)).unwrap();
         opt.backward_step(&loss).unwrap();
 
-        if n % 100 == 0{
+        {
             println!("{n}: {}", cost_sum/n as f32);
-            for row in yhat.to_vec2().unwrap(){
+            for row in &yhat.to_vec3().unwrap()[0]{
                 print!("{}", argmax(&row) as u8 as char)
             }
             println!();
